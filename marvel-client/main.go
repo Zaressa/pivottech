fe6d631f8f1388c69c6aa80bae9ec60ea3301026
Zaressa <109114618+Zaressa@users.noreply.github.com>
2022-12-03 23:56:07 +0000

    errors fixed

--- marvel-client/main.go
+++ marvel-client/main.go
@@ -1,8 +1,9 @@
 package main
 
-// marvel.NewClient()
-//client.GetCharacters()
+
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -12,49 +13,87 @@ import (
 	"github.com/joho/godotenv"
 )
 
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	publicKey := os.Getenv("marvel_public_key")
-	privateKey := os.Getenv("marvel_private_key")
+	publicKey := os.Getenv("MARVEL_PUBLIC_KEY")
+	privateKey := os.Getenv("MARVEL_PRIVATE_KEY")
 
 	client := marvelClient{
 		publicKey:  publicKey,
 		privateKey: privateKey,
 		httpClient: &http.Client{
-			Timeout: 20 * time.Second,
+			Timeout: 10 * time.Second,
 		},
 	}
-	charc, err := client.getCharacters() // this is where I want to call the function
-	if err != nil {
-		log.Println("action failed", err)
-		return
+	
 
-	}
-	log.Println(charc)
+type Character struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
 }
 
 type marvelClient struct {
 	publicKey  string
 	privateKey string
 	httpClient *http.Client
+	BaseURL    string
 }
 
-func (c *marvelClient) getCharacters() ([]Character, error) {
-	res, err := c.httpClient.Get("https://gateway.marvel.com/v1/public/characters") //contians the endpoint
+type CharacterResponse struct {
+	Code      int         `json:"code"`
+	Status    string      `json:"status"`
+	Etag      string      `json:"etag"`
+	AttributionHTML string `json:"attributionHTML"`
+	AttributionText string `json:"attributionText"`
+	Data      struct {
+		Offset      int         `json:"offset"`
+		Limit       int         `json:"limit"`
+		Total       int         `json:"total"`
+		Count       int         `json:"count"`
+		Results     []Character `json:"results"`
+	} `json:"data"`
+	
+}
+func (c *marvelClient) md5Hash(ts int64) string {
+	hasher := md5.New()
+	hasher.Write([]byte(ts + c.privateKey + c.publicKey))
+	return hex.EncodeToString(hasher.Sum(nil))
+}	
+
+func (c *marvelClient) md5Hash (ts, string) string {
+	ts := strconv.Itoa(ts)
+	hash := md5.Sum([]byte(ts + c.privateKey + c.publicKey))
+	return hex.EncodeToString(hash[:])
+
+}
+func (c *MarvelClient) signURL(url string) string {
+	ts := time.Now().Unix()
+	hash := c.md5Hash(ts)
+	return fmt.Sprintf("%s&ts=%d&apikey=%s&hash=%s", url, ts, c.PubKey, hash)
+}
+func (c *marvelClient) GetCharacters(limit int) ([]Character, error) {
+	limit := stronv.Itoa(limit)
+	url := c.BaseURL + "/characters?limit=" + limit
+	url = c.signURL(url)
+
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Println("action failed", err)
 		return nil, err
 	}
+	defer req.Body.Close()
 
-	defer res.Body.Close()
-
-	var characterResponse CharacterResponse
-	if err := json.NewDecoder(res.Body).Decode(&characterResponse); err != nil {
+	var CharacterResponse CharacterResponse
+	err = json.NewDecoder(req.Body).Decode(&CharacterResponse)
+	if err != nil {
 		return nil, err
-
 	}
-	return nil, nil
+	return CharacterResponse.Data.Results, nil
 }
+
+
+// Path: marvel-client/marvel.go
