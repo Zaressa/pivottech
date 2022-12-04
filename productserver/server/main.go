1818b59f63c0ef2cc849075f8c3dda83f0ecf619
Zaressa <109114618+Zaressa@users.noreply.github.com>
2022-12-04 04:49:18 +0000

    Updated seeder

--- productserver/server/main.go
+++ productserver/server/main.go
@@ -5,11 +5,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
-//this can be used across any func
+// this can be used across any func
 var products []product
 
 type product struct {
@@ -19,27 +20,61 @@ type product struct {
 	Description string `json:"description"`
 }
 
-func getProductsHandler(w http.ResponseWriter, r *http.Request) {
+func getProducts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(products); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
+
+}
+func getProductsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	p := productlookup(id)
+	if p == nil {
+		log.Printf("Product not found: %d\n", id)
+		return
+	}
+	if err := json.NewEncoder(w).Encode(p); err != nil {
+		log.Printf("Error encoding product: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+}
+
+func productlookup(id int) *product {
+	for _, p := range products {
+		if p.ID == id {
+			return &p
+		}
+	}
+	return nil
 }
 
 func initProducts() {
-	bs, err := os.ReadFile("products.json")
+	bs, err := os.ReadFile("cmd/seeder/products.json")
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := json.Unmarshal(bs, &products); err != nil {
-		log.Fatal((err))
+	err = json.Unmarshal(bs, &products)
+	if err != nil {
+		log.Fatal(err)
+		return
 	}
 }
 
 func main() {
 	initProducts()
 	r := mux.NewRouter()
-	// Routes consist of a path and a handler function.
+	r.HandleFunc("/products", getProducts).Methods("GET")
+	r.HandleFunc("/products/{id}", getProductsHandler).Methods("GET")
+
 	r.HandleFunc("/products", getProductsHandler)
 
 	// Bind to a port and pass our router in
